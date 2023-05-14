b864d2a8c9ad3a23184b51c8f5ae8d91a2842441
Hexanima <nicolasfilippelli22@gmail.com>
2023-05-14 19:54:14 -0300

    Micro desafio 1 hecho

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,4 +1,19 @@
-window.onload = function(){
+// Afanado de documentacion
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#examples
+function randomNum() {
+    min = Math.ceil(0);
+    max = Math.floor(99);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor() {
+    let r = randomNum();
+    let g = randomNum();
+    let b = randomNum();
+    return `#${r}${g}${b}`;
+}
+
+window.onload = function () {
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
@@ -8,5 +23,5 @@ window.onload = function(){
     formulario.classList.add('fondoCRUD');
 
 
-
+    titulo.addEventListener("mouseover", () => titulo.style.color = randomColor());
 }
